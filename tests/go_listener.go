2c1c6940104f37a4d6aacc7969b50d9eca568793
Miklin (remote) <MiklinMA@gmail.com>
2018-04-26 17:31:44 +0500

    API proxy GO test

--- tests/go_listener.go
+++ tests/go_listener.go
@@ -3,37 +3,85 @@ package main
 import (
 	"fmt"
     "time"
+    "strings"
+    "strconv"
 
-	"github.com/nats-io/go-nats"
     "net/http"
     "io/ioutil"
+    "encoding/json"
+
+	"github.com/nats-io/go-nats"
 )
 
 var count int
 var err error
 const server = "nats://nero:4222"
-const web_server = "nats://nero:2030"
+const web_server = "http://127.0.0.1:2030/"
 // const server = nats.DefaultURL
 var nc *nats.Conn
+
+type Packet struct {
+    Method string
+    Data map[string]string
+    Header map[string]string
+    Raw string
+}
 
 func on_subscribe(m *nats.Msg) {
     fmt.Printf("GET: %s %s\n", m.Subject, m.Reply)
     nc.Publish(m.Reply, []byte("found"))
 
-    res, err := http.Get("http://nero:2030/api_get_events_scheduled_master?date=2018-04-26")
+    var p Packet
+
+    err = json.Unmarshal(m.Data, &p)
     if nil != err {
-        fmt.Printf("ERROR: %s %s %s\n", m.Subject, m.Reply, err)
+        fmt.Printf("ERROR: %s %s\n", m.Subject, err)
+        nc.Publish(m.Reply, []byte("400"))
+        return
+    }
+
+    method := p.Method
+    arr := strings.Split(method, ".")
+    path := strings.Join(arr[:len(arr)-1], "_")
+    method = arr[len(arr)-1]
+    method = strings.ToUpper(method)
+
+    uri := web_server + path
+
+    req, _ := http.NewRequest(method, uri, nil)
+    q := req.URL.Query()
+    for k, v := range p.Data {
+        q.Add(k, v)
+    }
+    req.URL.RawQuery = q.Encode()
+
+    for k, v := range p.Header {
+        req.Header.Add(k, v)
+    }
+    // fmt.Println(m.Subject, req.URL.String())
+
+    res, err := http.DefaultClient.Do(req)
+
+    if nil != err {
+        fmt.Printf("ERROR: %s %s\n", m.Subject, err)
+        nc.Publish(m.Reply, []byte("502"))
         return
     }
     response, err := ioutil.ReadAll(res.Body)
     res.Body.Close()
     if nil != err {
-        fmt.Printf("ERROR: %s %s %s\n", m.Subject, m.Reply, err)
+        fmt.Printf("ERROR: %s %s\n", m.Subject, err)
+        nc.Publish(m.Reply, []byte("502"))
         return
     }
-    nc.Publish(m.Reply, []byte(response))
-    fmt.Printf("Finish: %s %s\n", m.Subject, m.Reply)
-    count = count + 1
+    if res.StatusCode == 200 {
+        nc.Publish(m.Reply, []byte(response))
+        fmt.Printf("Done: %s %d\n", m.Subject, res.StatusCode)
+        // count = count + 1
+    } else {
+        nc.Publish(m.Reply, []byte(strconv.Itoa(res.StatusCode)))
+        fmt.Printf("Finish: %s %d %s\n", m.Subject, res.StatusCode, string(response))
+    }
 }
 
 func counter() {
@@ -57,7 +105,7 @@ func main() {
         go on_subscribe(m)
     })
 
-    go counter()
+    // go counter()
 
     <-forever
 
