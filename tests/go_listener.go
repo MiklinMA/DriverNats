1abac2216c22357100bdb75e3547d53ef0f2170f
Михаил Миклин <MiklinMA@gmail.com>
2018-04-26 16:03:39 +0500

    go proxy

--- tests/go_listener.go
+++ tests/go_listener.go
@@ -2,36 +2,62 @@ package main
 
 import (
 	"fmt"
-	"time"
+    "time"
 
 	"github.com/nats-io/go-nats"
+    "net/http"
+    "io/ioutil"
 )
 
 var count int
+var err error
 const server = "nats://nero:4222"
+const web_server = "nats://nero:2030"
 // const server = nats.DefaultURL
+var nc *nats.Conn
 
 func on_subscribe(m *nats.Msg) {
+    fmt.Printf("GET: %s %s\n", m.Subject, m.Reply)
+    nc.Publish(m.Reply, []byte("found"))
+
+    res, err := http.Get("http://nero:2030/api_get_events_scheduled_master?date=2018-04-26")
+    if nil != err {
+        fmt.Printf("ERROR: %s %s %s\n", m.Subject, m.Reply, err)
+        return
+    }
+    response, err := ioutil.ReadAll(res.Body)
+    res.Body.Close()
+    if nil != err {
+        fmt.Printf("ERROR: %s %s %s\n", m.Subject, m.Reply, err)
+        return
+    }
+    nc.Publish(m.Reply, []byte(response))
+    fmt.Printf("Finish: %s %s\n", m.Subject, m.Reply)
     count = count + 1
-    fmt.Printf("GET: %s %s\n", m.Subject, string(m.Data))
-    time.Sleep(3 * time.Second)
-    fmt.Printf("Finish: %s %s\n", m.Subject, string(m.Data))
+}
+
+func counter() {
+    for true {
+        fmt.Println(count)
+        time.Sleep(5 * time.Second)
+    }
 }
 
 func main() {
-    nc, err := nats.Connect(server)
+    nc, err = nats.Connect(server)
     if nil != err {
         fmt.Println(err)
     }
     fmt.Println("Connected to:", server)
     defer nc.Close()
-    defer fmt.Print(count)
 
     forever := make(chan bool)
 
-    nc.Subscribe(">", func(m *nats.Msg) {
+    nc.Subscribe("api.>", func(m *nats.Msg) {
         go on_subscribe(m)
     })
+
+    go counter()
 
     <-forever
 
